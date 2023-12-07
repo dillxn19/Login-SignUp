026d6a0216a2d4f7f0a6e794beb15bb102a005b8
Dillan <dillanharan@gmail.com>
2023-12-06 22:15:47 -0800

    created sign up feature

---
+++ main.py
@@ -0,0 +1,29 @@
+import sqlite3
+conn = sqlite3.connect("users.db")
+c = conn.cursor()
+while True:
+    ask = input("To login press 1 \nTo signup press 2\n")
+    if ask == "1":
+        un = input("Username: ")
+        pw = input("Password: ")
+        c.execute(f"select username, password from user_info where username = '{un}' and password = '{pw}'")
+        result = c.fetchone()
+        if result != None:
+            print("Login Successful!")
+        else:
+            print("Login Failed")
+
+    elif ask == "2":
+        new_name = input("New Name: ")
+        new_un = input("New Username: ")
+        new_pw = input("New Password: ")
+        c.execute(f"select username from user_info where username = '{new_un}'")
+        result2 = c.fetchone()
+        if result2 != None:
+            print("Username has been taken")
+        else:
+            c.execute(f"insert into user_info values('{new_name}', '{new_un}', '{new_pw}')")
+        conn.commit()
+        
+conn.commit()
+
